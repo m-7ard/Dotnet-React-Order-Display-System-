49f5fc8962686f0a8dbaa0565a571d13fbcc55b8
m <maximsevenardfassers@gmail.com>
2025-01-30 20:20:58 +0100

    vite base fix;

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: '/react/',
+    base: mode === "production" ? "/react/" : "/",
     build: {
         outDir: 'react'
     }
-});
+}));
