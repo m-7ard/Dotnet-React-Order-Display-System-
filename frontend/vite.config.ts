d2979b9d96c4342e64da533dd4ed39a77e9dd684
m <maximsevenardfassers@gmail.com>
2025-04-01 01:43:07 +0200

    hetzner url test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: mode === "production" ? "/react/" : "/",
+    base: mode === "production" ? "/Dotnet-React-Order-Display-System-/react/" : "/",
     build: {
         outDir: "react",
     },
